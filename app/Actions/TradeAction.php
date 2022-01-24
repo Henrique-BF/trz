1a9358e2a5e0e6e8325468b3b20fb25ae134972d
Henrique Borges <henrybf10@hotmail.com>
2022-01-24 11:31:58 -0300

    Update trade features

--- app/Actions/TradeAction.php
+++ app/Actions/TradeAction.php
@@ -4,6 +4,7 @@ namespace App\Actions;
 
 use App\Models\Item;
 use App\Models\Survivor;
+use Exception;
 use Illuminate\Support\Facades\DB;
 
 class TradeAction
@@ -13,38 +14,20 @@ class TradeAction
 
     public function hundle(Survivor $survivor_1, Survivor $survivor_2, $ids_and_qty_1, $ids_and_qty_2)
     {
-        return DB::transaction(function() use($survivor_1, $survivor_2, $ids_and_qty_1, $ids_and_qty_2){
-            $items_for_trade_survivor_1 = $this->takeItems($ids_and_qty_1);
-            $items_for_trade_survivor_2 = $this->takeItems($ids_and_qty_2);
-            
-            $total_trade_survivor_1 = $this->calculateTotalPoints($items_for_trade_survivor_1, $ids_and_qty_1);
-            $total_trade_survivor_2 = $this->calculateTotalPoints($items_for_trade_survivor_2, $ids_and_qty_2);
-            if($total_trade_survivor_1 != $total_trade_survivor_2)
-            {
-                return response()->json([
-                    'message' => 'NOT ENOUGH POINTS STRANGER!!! Survivor 1 offer '.$total_trade_survivor_1.' points and survivor 2 offer '.$total_trade_survivor_2.' points for trade.'
-                ], 422);
-            }
+        $this->compareAmountOfPoints($ids_and_qty_1, $ids_and_qty_2);
 
-            $inventory_survivor_1 = $survivor_1->inventory;
-            $inventory_survivor_2 = $survivor_2->inventory;
-            
-            $error = $this->compareItems($inventory_survivor_1, $ids_and_qty_1); 
-            if($error) return response()->json([
-                'message' => 'NOT ENOUGH ITEMS! Stranger 1'
-            ], 422); 
+        $inventory_survivor_1 = $survivor_1->inventory;
+        $inventory_survivor_2 = $survivor_2->inventory;
+        
+        $this->checkInventory($inventory_survivor_1, $ids_and_qty_1);
+        $this->checkInventory($inventory_survivor_2, $ids_and_qty_2);
 
-            $error = $this->compareItems($inventory_survivor_2, $ids_and_qty_2);
-            if($error) return response()->json([
-                'message' => 'NOT ENOUGH ITEMS! Stranger 2'
-            ], 422);
-
+        return DB::transaction(function() use($inventory_survivor_1, $inventory_survivor_2, $ids_and_qty_1, $ids_and_qty_2){
             $this->makeTrade($inventory_survivor_1, $ids_and_qty_2, $ids_and_qty_1);
             $this->makeTrade($inventory_survivor_2, $ids_and_qty_1, $ids_and_qty_2);
 
             return ['message' => 'HEHEHEA! Thank you. Is that all, stranger? Come back any time.'];
         });
-        
     }
 
     private function makeTrade($inventory, $ids_and_qty_in, $ids_and_qty_out)
@@ -62,6 +45,36 @@ class TradeAction
             $item->pivot->qty == $id_qty_out['qty']
                 ? $inventory->items()->detach($id_qty_out['id'])
                 : $item->pivot->update(['qty' => $item->pivot->qty - $id_qty_out['qty']]);
+        }
+    }
+
+    private function compareAmountOfPoints($ids_and_qty_1, $ids_and_qty_2)
+    {
+        $items_for_trade_survivor_1 = $this->takeItems($ids_and_qty_1);
+        $items_for_trade_survivor_2 = $this->takeItems($ids_and_qty_2);
+        
+        $total_trade_survivor_1 = $this->calculateTotalPoints($items_for_trade_survivor_1, $ids_and_qty_1);
+        $total_trade_survivor_2 = $this->calculateTotalPoints($items_for_trade_survivor_2, $ids_and_qty_2);
+
+        if($total_trade_survivor_1 != $total_trade_survivor_2)
+        {
+            throw new Exception(
+                message: 
+                    'NOT ENOUGH POINTS STRANGER!!! Survivor 1 offer '
+                    .$total_trade_survivor_1.' points and survivor 2 offer '
+                    .$total_trade_survivor_2.' points for trade.'
+            );
+        }
+    }
+
+    private function checkInventory($inventory_survivor_1, $ids_and_qty_1)
+    {
+        $error = $this->compareItems($inventory_survivor_1, $ids_and_qty_1); 
+        if($error)
+        {
+            throw new Exception(
+                message: 'NOT ENOUGH ITEMS! Stranger ' . $inventory_survivor_1->survivor_id
+            );
         }
     }
 
--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Actions\TradeAction;
 use App\Http\Requests\TradeRequest;
+use App\Models\Item;
 use App\Models\Survivor;
+use Exception;
 
 class TradeController extends Controller
 {
+    public function indexItems()
+    {
+        return Item::all();
+    }
+
     public function trade(TradeRequest $request, TradeAction $action)
     {
         $survivor_1 = Survivor::find($request->survivor_1_id);
@@ -18,6 +25,12 @@ class TradeController extends Controller
         abort_if($survivor_1->infected->was_infected, 403, 'Danger. Survivor infected!!');
         abort_if($survivor_2->infected->was_infected, 403, 'Danger. Survivor infected!!');
 
-        return $action->hundle($survivor_1, $survivor_2, $items_survivor_1, $items_survivor_2);
+        try{
+            $response = $action->hundle($survivor_1, $survivor_2, $items_survivor_1, $items_survivor_2);
+        }
+        catch(Exception $exception){
+            return response()->json(['message' => $exception->getMessage()], 422);
+        }
+        return $response;
     }
 }
